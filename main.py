41a6273d859e1fb305ab78a9b25a44d60605620e
flatypus [mac] <yhc3141@gmail.com>
2025-01-13 00:16:29 -0800

    fix: timing

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ async def update_history():
             "artist": song["artists"][0]["name"],
             "thumbnail": song["thumbnails"][0]["url"],
             "duration_s": song["duration_seconds"],
-            "started": current_time,
+            "started": current_time + RELOAD_TIME / 2 + 1,
             "url": f"https://music.youtube.com/watch?v={song['videoId']}"
         }
     elif listening_to and listening_to["started"] + listening_to["duration_s"] < current_time:
