ee695ee88e91caa0acaf7d3e11e53508200e6d4c
flatypus [mac] <yhc3141@gmail.com>
2025-01-12 23:11:39 -0800

    fix: should save bandwidth

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
-from contextlib import asynccontextmanager
 from dotenv import load_dotenv
-from fastapi import FastAPI
+from fastapi import BackgroundTasks, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import StreamingResponse
 from ytmusicapi import YTMusic
 from ytmusicapi.auth.oauth import OAuthCredentials
-import asyncio
-import json
-import os
-import time
+import os, time, json, asyncio
 
 load_dotenv()
 
@@ -27,40 +23,43 @@ else:
 if CLIENT_ID is None or CLIENT_SECRET is None:
     raise Exception("CLIENT_ID and CLIENT_SECRET must be provided")
 
+RELOAD_TIME = 5
+
 oauth_credentials=OAuthCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
 ytmusic = YTMusic("oauth.json", BRAND, oauth_credentials=oauth_credentials)
 
 listening_to = None
+last_update = time.time()
+connections = []
 
-async def run_background():
-    global listening_to
-    while True:
-        await asyncio.sleep(5)
-        try:
-            history = ytmusic.get_history()
-            last_song = ytmusic.get_playlist(playlistId="FEmusic_history", limit=1)
-            print("Last song:", last_song)
-        except Exception as e:
-            print("Error:", e)
-            continue
+async def update_history():
+    global listening_to, last_update
+    
+    current_time = time.time()
+    if current_time - last_update < RELOAD_TIME: 
+        return
+    last_update = current_time
+    
+    try:
+        history = ytmusic.get_history()
+        last_song = ytmusic.get_playlist(playlistId="FEmusic_history", limit=1)
+        print("Last song:", last_song)
+    except Exception as e:
+        print("Error:", e)
+        return
+    
+    song = history[0]
+    if listening_to is None or listening_to["title"] != song["title"]:
+        listening_to = {
+            "title": song["title"],
+            "artist": song["artists"][0]["name"],
+            "thumbnail": song["thumbnails"][0]["url"],
+            "duration_s": song["duration_seconds"],
+            "started": time.time(),
+            "url": f"https://music.youtube.com/watch?v={song['videoId']}"
+        }
         
-        song = history[0]
-        if listening_to is None or listening_to["title"] != song["title"]:
-            listening_to = {
-                "title": song["title"],
-                "artist": song["artists"][0]["name"],
-                "thumbnail": song["thumbnails"][0]["url"],
-                "duration_s": song["duration_seconds"],
-                "started": time.time(),
-                "url": f"https://music.youtube.com/watch?v={song['videoId']}"
-            }
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    asyncio.create_task(run_background())
-    yield
-        
-app = FastAPI(lifespan=lifespan)
+app = FastAPI()
 
 origins = ["*"]
 
@@ -76,22 +75,20 @@ app.add_middleware(
 def root():
     return "Youtube Music live listening API"
 
-
 @app.get("/live")
 async def live():
-    # create generator that yields data when new song is played
     async def streamer_live():
         while True:
+            await update_history()
             yield f"data: {json.dumps(listening_to)}\n\n"
-            await asyncio.sleep(5)
+            await asyncio.sleep(RELOAD_TIME)
 
     return StreamingResponse(streamer_live())
 
-
 @app.get("/last")
 async def last():
+    await update_history()
     return json.dumps(listening_to)
-
 
 if __name__ == "__main__":
     import uvicorn
